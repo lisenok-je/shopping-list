24d030fa01e41c256a385745da01ec6295cb2859
lisenok-je <jaliz1994@mail.ru>
2024-01-10 21:49:47 +0300

    написали код js

--- script.js
+++ script.js
@@ -7,3 +7,26 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const inputWrapper = document.querySelector('.input-wrapper')
+const inputEnter = document.querySelector('#input');
+const itemsContainer = document.querySelector(.items);
+
+inputEnter.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const productText = inputEnter.value;
+
+        const newItems = document.createElement('div');
+        newItems.classList.add('product');
+        newItems.textContent = productText;
+
+        itemsContainer.append(productText);
+
+        newItems.addEventListener('click', () => {
+            newItems.classList.toggle('done');
+        })
+
+        inputEnter.value = ' ';
+
+    }
+});
